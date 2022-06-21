5b5daeeecc0e8a2f7dd2bf892bbce445bf5ef979
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-21 08:48:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice21June2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert first number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Insert second number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 > number2)
+            {
+                Console.WriteLine("Maximum value of the two is: {0}", number1);
+            }
+            else if (number1 < number2)
+            {
+                Console.WriteLine("Maximum value of the two is: {0}", number2);
+            }
+            else if (number1 == number2)
+            {
+                Console.WriteLine("Two numbers are equal");
+            }
+            else
+            {
+                Console.WriteLine("hmm?");
+            }
+        }
+    }
+}
